d77744058593b9da707003e02fa7ab54aec66494
Raji Gali <raji@qxf2.com>
2018-09-25 14:28:18 +0530

    return hello everyone python code

---
+++ hello.py
@@ -0,0 +1,20 @@
+"""
+GD practice flask app
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    "home page"
+    return "Hello  Everyone"
+
+#---START
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=8000)
+
+
+
+#
